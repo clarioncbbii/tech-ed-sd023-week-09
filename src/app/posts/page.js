bfe5156f2c312f1b69beaf62e89f706707d33fcb
Nene <nene.nnoruka@gmail.com>
2026-01-29 11:39:34 +0000

    bug fixing

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,2 +1,10 @@
 //TODO: Render a list of all posts
 //- you could also render the user who posted them
+
+export default function PostSPage() {
+  return (
+    <>
+      <h1>All posts</h1>
+    </>
+  );
+}
