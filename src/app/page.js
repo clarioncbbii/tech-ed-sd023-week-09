6be422ae417310c000847a03eff35b16d30fdd84
Nene <nene.nnoruka@gmail.com>
2026-02-02 22:00:12 +0000

    feat: links updated

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,12 @@ export default function HomePage() {
   return (
     <>
       <h1>Home Page</h1>
+      <Link href={`/posts`} className="text-blue-600">
+        ALL POST
+      </Link>
+      <Link href={`/profile/:username`} className="text-blue-600">
+        YOUR PROFILE
+      </Link>
     </>
   );
 }
